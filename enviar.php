b64edc2c597cd2209be5e40ea21088406ddf5e59
Ananda-28 <89762894+Ananda-28@users.noreply.github.com>
2021-08-30 01:22:55 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+$telefono = $_POST ['telefono'];
+$pedido = $_POST ['pedido'];
+
+$destinatario = "AL11075@abrahamlincoln.edu.pe";
+$asunto = "Mini Dulce Pedido";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $email \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Pedido: $pedido";
+
+mail($destinatario, $asunto, $carta)
+header('Location:envio')
+?>
